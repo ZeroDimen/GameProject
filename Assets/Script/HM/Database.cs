93b7a1d5836bf2c5aea493ab41e59e75085924de
khm6211 <khm6211@gmail.com>
2024-06-10 20:16:16 +0900

    Install Localization Pacakage
    유니티 Localization 패키지 설치 후 관련 파일 생성

---
+++ Assets/Script/HM/Database.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Database : MonoBehaviour  // 서버에 데이터를 저장하기 위한 스크립트
+{
+    private string path;
+    public void DataInput()   //Bullet_Game에 대이터를 인자로 함수 UnityWebRequestGETTest를 호출하여 서버에 데이터를 저장한다.
+    {
+        //여기서 로컬에 있는 파일 읽어서 서버로 보내야함
+        path = Application.persistentDataPath + "/";
+        string[] json = new string[3];
+        for (int i = 0; i < 3; i++)
+        {
+            if (File.Exists(Data_Manager.instance.path + $"{i}"))
+            {
+                Debug.Log($"{i} Load Success");
+                json[i] = File.ReadAllText(path + i.ToString());
+            }
+            else
+            {
+                json[i] = null;
+            }
+        }
+        StartCoroutine(UnityWebRequestGetTest(json));
+    }
+
+    IEnumerator UnityWebRequestGetTest(string[] json)
+    {
+
+        // GET 방식
+        string url = $"http://113.198.229.155:4007/update?userID=khm&userPW=gusals&json1='{json[0]}'&json2='{json[1]}'&json3='{json[2]}'";
+        Debug.Log(url);
+
+        // UnityWebRequest에 내장되있는 GET 메소드를 사용한다.
+        UnityWebRequest www = UnityWebRequest.Get(url);
+
+        yield return www.SendWebRequest();  // 응답이 올때까지 대기한다.
+
+        if (www.error == null)  // 에러가 나지 않으면 동작.
+        {
+            Debug.Log(www.downloadHandler.text);
+        }
+        else
+        {
+            Debug.Log(www.error);
+        }
+    }
+}
