0cf934d4c8eeaf128564eabd046f5ea17aef203b
khm6211 <khm6211@gmail.com>
2024-06-15 17:15:52 +0900

    Add UI, Login System, Localization
    UI, 로그인시스템, 대사 추가

---
+++ Assets/Script/HM/Test.cs
@@ -0,0 +1,29 @@
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using System.Linq;
+// using UnityEditor;
+// using UnityEditor.Localization;
+// using UnityEngine;
+// using UnityEngine.Localization.Components;
+// using UnityEngine.Localization.Tables;
+// using UnityEngine.UIElements;
+//
+// public class Test : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     public LocalizeStringEvent b1;
+//     public LocalizationTable a1;
+//     public LocalizationGroup g1;
+//     public StringTable t1;
+//     public StringTableCollection c1;
+//     public Button btn;
+//     private void Update()
+//     {
+//     }
+//
+//     private void Start()
+//     {
+//         b1.SetEntry("UI_Continue");
+//     }
+// }
--- Assets/Script/HM/UIManager.cs
+++ Assets/Script/HM/UIManager.cs
@@ -3,9 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.PlayerLoop;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
-enum GameStatus
+public enum GameStatus
 {
     Title,
     Ingame,
@@ -15,12 +20,20 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager instance = null;
     public GameObject EscapeMenu;
-    public GameObject LoginPanel;
     public GameObject SlotMenu;
-    public TMP_Text Idtext;
-    public TMP_Text Pwtext;
+    public GameObject Login;
+    public GameObject Signup;
+    public GameObject Player;
     private bool IsEscapeMenuOpen;
-    private GameStatus _gameStatus;
+    public GameStatus gameStatus;
+    public TMP_InputField InputIdField_Signin;
+    public TMP_InputField InputPwField_Signin;
+    public TMP_InputField InputIdField_Login;
+    public TMP_InputField InputPwField_Login;
+    public Button Continue;
+    public Button Setting;
+    public Button Exit;
+    public Button LoginBtn;
 
     private void Awake()
     {
@@ -35,26 +48,102 @@ public class UIManager : MonoBehaviour
         }
         EscapeMenu.SetActive(false);
         SlotMenu.SetActive(false);
-        LoginPanel.SetActive(true);
-        _gameStatus = GameStatus.Title;
+        Signup.SetActive(false);
+        Login.SetActive(true);
+        
+    }
+
+    private void Start()
+    {
+        gameStatus = GameStatus.Title;
+        Player = GameObject.Find("Player");
     }
 
     private void Update()
     {
-        //그거 뭐야 이스케이프 키 눌렀을때 반응
+        if(gameStatus == GameStatus.Ingame)
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (EscapeMenu.activeSelf)
+                {
+                    EscapeMenu.SetActive(false);
+                }
+                else
+                {
+                    EscapeMenu.SetActive(true);
+                    Continue.Select();
+                }
+            }
+        }
+
+        if (InputIdField_Login.isFocused||InputPwField_Login.isFocused)
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                InputPwField_Login.Select();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Debug.Log("click enter");
+                LoginBtn.onClick.Invoke();
+            }
+            
+            
+        }
+
+        if (InputPwField_Signin.isFocused || InputPwField_Signin.isFocused)
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                InputPwField_Signin.Select();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                string s1 = InputIdField_Login.text;
+                string s2 = InputPwField_Login.text;
+                GetComponent<Database>().CreateAccount(s1, s2);
+            }
+        }
     }
 
     public void LoginButton()
     {
-        GameManager.instance.id = Idtext.text;
-        GameManager.instance.pw = Pwtext.text;
-        SlotMenu.SetActive(true);
+        GameManager.instance.id = InputIdField_Login.text;
+        GameManager.instance.pw = InputPwField_Login.text;
+        
         //서버에서 불러오기?
-        LoginPanel.SetActive(false);
+        GetComponent<Database>().LoadData();
+        
+        GetComponent<SaveData>().LoadData();
+        SlotMenu.SetActive(true);
+        GetComponent<SaveData>().SlotLoad();
+        Login.SetActive(false);
+        
         //this.gameObject.SetActive(false);
     }
 
-    public void ExitButton()
+    public void LoginToSignUp()
+    {
+        Login.SetActive(false);
+        Signup.SetActive(true);
+    }
+
+    public void SignupToLogin()
+    {
+        Signup.SetActive(false);
+        Login.SetActive(true);
+    }
+
+    public void ExitButtonInGame()
+    {
+        //현재 세이브 파일 저장 불가능함 일단은 슬롯 메뉴 창으로 돌아가는 걸로 or 타이틀화면 나오면 타이틀 화면으로 ㄱㄱ
+        
+    }
+
+    public void ContinueButton()
     {
         
     }
